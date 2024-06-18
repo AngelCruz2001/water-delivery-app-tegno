1f92e7c01388d22db34f7a5bddeb3fb2785494c8
RickSandov <i.s.ricardo.sandoval@gmail.com>
2024-06-17 18:44:38 -0600

    create client map

--- src/providers/WebsocketLocationProvider.tsx
+++ src/providers/WebsocketLocationProvider.tsx
@@ -22,9 +22,9 @@ export const WebsocketLocationProvider = ({ children }: Props) => {
     // User 
     const user = useUserStore((state) => state.user);
 
-    // const url = "ws://localhost:8080/ws";
+    const url = "ws://localhost:8081/ws";
     // const url = "ws://192.168.1.85:8080/ws";
-    const url = "ws://water-delivery-backend.onrender.com/ws";
+    // const url = "wss://water-delivery-backend.onrender.com/ws";
 
 
     useEffect(() => {
