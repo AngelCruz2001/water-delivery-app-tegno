7115248ce55cd46169aa36cd836552b12f14f611
RickSandov <i.s.ricardo.sandoval@gmail.com>
2024-06-26 16:27:13 -0600

    Routes Changes

--- src/store/routes/api/postOrdersBatch.ts
+++ src/store/routes/api/postOrdersBatch.ts
@@ -1,20 +1,16 @@
-import {TCreateOrderDto, TDisplayOrder} from '../../../interfaces/order';
+import {TCreateOrderDto} from '../../../interfaces/order';
 import {api, getToken} from '../../../presentation/api/api';
 
 export const postOrdersBatch = async (
   createOrders: TCreateOrderDto[],
 ): Promise<any> => {
   try {
-    const {data} = await api.post<TDisplayOrder[]>(
-      '/orders/batch',
-      createOrders,
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          authorization: await getToken(),
-        },
+    const {data} = await api.post('/orders/batch', createOrders, {
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: await getToken(),
       },
-    );
+    });
     return data;
   } catch (error) {
     console.log('pst route error: ', {error});
