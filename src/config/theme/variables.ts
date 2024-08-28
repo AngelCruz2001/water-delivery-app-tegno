15e9667e5b68cae013e71d9a35fadd1bef2b0c14
Ángel Cruz García <34138352+AngelCruz2001@users.noreply.github.com>
2024-08-28 11:44:01 -0600

    Update variables.ts

--- src/config/theme/variables.ts
+++ src/config/theme/variables.ts
@@ -1,2 +1,2 @@
-export const iOSGoogleApiKey = 'AIzaSyDZgeinFHDD0A8Kccpr4EEDD0REnwiQzQU';
-export const androidGoogleApiKey = 'AIzaSyAwfNyZzepLK6esCEGvXO4tPpVv6k-dNlA';
+export const iOSGoogleApiKey = '';
+export const androidGoogleApiKey = '';
